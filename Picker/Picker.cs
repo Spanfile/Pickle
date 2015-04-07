34d0f99bf0ff8e8b8090e0f48009407171b60547
Spanfile <santeri.kasvi@gmail.com>
2015-04-07 16:24:39 +0300

    Right I should commit these

---
+++ Picker/Picker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Picker
+{
+    public sealed class Picker<T> where T : struct
+    {
+        public Picker()
+        {
+
+        }
+
+        public void AddItem(T item, int chance)
+        {
+
+        }
+    }
+}
