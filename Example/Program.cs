6705360fe2cf700caba125b4e3fafbc85d9ddedb
Spanfile <santeri.kasvi@gmail.com>
2015-08-06 00:59:46 +0300

    New Example

--- Example/Program.cs
+++ Example/Program.cs
@@ -20,40 +20,68 @@ namespace Example
         static void Main(string[] args)
         {
             // create a new picker that returns MyEnums
-            Picker<MyEnum> picker = new Picker<MyEnum>();
+            var picker = new Picker<MyEnum>();
 
-            // add the four enum values to it with their probabilities as percentages
-            picker.AddItem(MyEnum.Item1, 50);
-            picker.AddItem(MyEnum.Item2, 20);
-            picker.AddItem(MyEnum.Item3, 15);
-            picker.AddItem(MyEnum.Item4, 15);
+            // add the four weighted enum values to it
+            picker.AddItem(MyEnum.Item1, 1);
+            picker.AddItem(MyEnum.Item2, 2);
+            picker.AddItem(MyEnum.Item3, 3);
+            picker.AddItem(MyEnum.Item4, 4);
+            // the weights total to 10, meaning that
+            // MyEnum.Item1 has a chance of 1/10 to be returned
+            // MyEnum.Item2 has a chance of 2/10 to be returned
+            // MyEnum.Item3 has a chance of 3/10 to be returned
+            // MyEnum.Item4 has a chance of 4/10 to be returned
 
             // take a value from the picker and display it
-            MyEnum item = picker.NextItem();
-            Console.WriteLine("You got {0}!", item);
+            var item = picker.NextItem();
+            Console.WriteLine($"You got {item}!");
 
             // take multiple values and display them
-            MyEnum[] items = picker.NextItems(10).ToArray();
-            Console.WriteLine("You also got {0} and {1}!", String.Join(", ", items.Take(9)), items.Last());
+            var items = picker.NextItems(5).ToArray();
+            Console.WriteLine($"You also got {string.Join(", ", items.Take(4))} and {items.Last()}!");
 
             // create a new categorised picker
             // you need to give a method that takes in your item and returns a name for that item
             // the method below simply calls .ToString() on the item
-            CategorisedPicker<MyEnum> catPick = new CategorisedPicker<MyEnum>(m => m.ToString());
+            var catPick = new CategorisedPicker<MyEnum>(m => m.ToString());
 
-            // add a category called "Another" to a category called "Test". the picker will add the missing categories automatically.
+            // add a category called "Another" to a category called "Test". the picker will add the missing category "Test" automatically.
             catPick.AddCategory("Test/Another");
+            catPick.AddCategory("Test/YetAnother");
 
-            // add a category called "MoreTest" to the previously added category. this time, the picker will throw an exception if the path contains missing categories.
+            // add a category called "MoreTest" to the previously added category "Another". this time, the picker will throw an exception if the path contains missing categories.
             catPick.AddCategory("Test/Another/MoreTest", false);
+            // the following would fail
+            // catPick.AddCategory("Test/Invalid/WhatAreYouDoing", false);
 
-            // add two items to the "Another" category, both with 50% chance of being picked
-            catPick.AddItem("Test/Another/MoreTest", MyEnum.Item1, 50);
-            catPick.AddItem("Test/Another/MoreTest", MyEnum.Item2, 50);
+            // add two items to the "Another/MoreTest" category, both with equal weight
+            catPick.AddItem("Test/Another/MoreTest", MyEnum.Item1, 1);
+            catPick.AddItem("Test/Another/MoreTest", MyEnum.Item2, 1);
+
+            // add two items to the "Test/YetAnother" category, both with equal weight
+            catPick.AddItem("Test/YetAnother", MyEnum.Item3, 1);
+            catPick.AddItem("Test/YetAnother", MyEnum.Item4, 1);
+
+            // right now, our category hierarchy looks like this
+            /*
+            Test
+                Another
+                    MoreTest
+                        Item: Item1, weight: 1
+                        Item: Item2, weight: 1
+                YetAnother
+                    Item: Item3, weight: 1
+                    Item: Item4, weight: 1
+            */
 
             // pick one item from the picker
             // this will go through all categories, and picks an item from the first category it find that contains items
-            Console.WriteLine(catPick.NextItem());
+            // looking at our hierarchy above, the first category that contains items is MoreTest
+            Console.WriteLine($"From Test/Another/MoreTest: {catPick.NextItem()}");
+
+            // pick one item from the picker, this time specifying a certain category of where to find items from
+            Console.WriteLine($"From Test/YetAnother: {catPick.NextItem("Test/YetAnother")}");
 
             Console.ReadKey();
         }
